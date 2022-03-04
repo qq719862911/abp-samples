c76e32d9ff2c26b03e5ff717e35f9e820ee790ca
haiyiguoHarry <haiyiguo@gammonconstruction.com>
2022-03-04 09:48:25 +0800

    update connection type by password and username

--- RabbitMqEventBus/App1/App1Module.cs
+++ RabbitMqEventBus/App1/App1Module.cs
@@ -1,6 +1,7 @@
 ﻿using Volo.Abp.Autofac;
 using Volo.Abp.EventBus.RabbitMq;
 using Volo.Abp.Modularity;
+using Volo.Abp.RabbitMQ;
 
 namespace App1
 {
@@ -12,6 +13,17 @@ namespace App1
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
+           
+            Configure<AbpRabbitMqOptions>(options =>
+            {
+                options.Connections.Default.DispatchConsumersAsync = true;
+                options.Connections.Default.UserName = "haiyiguo";
+                options.Connections.Default.Password = "haiyiguo";
+                options.Connections.Default.HostName = "192.168.26.106";
+                options.Connections.Default.Port = 5672;
+              // if the port is default ,not to set, the default port is 5672
+
+            });
             Configure<AbpRabbitMqEventBusOptions>(options =>
             {
                 options.ClientName = "TestApp1";
--- RabbitMqEventBus/App2/App2Module.cs
+++ RabbitMqEventBus/App2/App2Module.cs
@@ -1,6 +1,7 @@
 ﻿using Volo.Abp.Autofac;
 using Volo.Abp.EventBus.RabbitMq;
 using Volo.Abp.Modularity;
+using Volo.Abp.RabbitMQ;
 
 namespace App2
 {
@@ -12,6 +13,19 @@ namespace App2
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
+            Configure<AbpRabbitMqOptions>(options =>
+            {
+                
+               
+                options.Connections.Default.DispatchConsumersAsync = true;
+                options.Connections.Default.HostName = "192.168.26.106";
+                
+                options.Connections.Default.UserName = "haiyiguo";
+                options.Connections.Default.Password = "haiyiguo";
+                options.Connections.Default.VirtualHost = "/";
+                options.Connections.Default.Port = 5672;
+                // if the port is default ,not to set, the default port is 5672 rather web port 15672
+            });
             Configure<AbpRabbitMqEventBusOptions>(options =>
             {
                 options.ClientName = "TestApp2";
